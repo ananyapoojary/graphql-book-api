8455afac8d0e1e2a80dbbd203d9c5e2cf360f5c2
ananyapoojary <ananyaarunpoojary@gmail.com>
2025-02-12 16:03:02 +0530

    Added MongoDB models and GraphQL queries/mutations

---
+++ models/Book.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  genre: { type: String },
+  authorId: { type: mongoose.Schema.Types.ObjectId, ref: "Author", required: true },
+});
+
+module.exports = mongoose.model("Book", bookSchema);
--- schema.js
+++ schema.js
@@ -1,16 +1,57 @@
 const { gql } = require("apollo-server-express");
+const Book = require("./models/Book");
+const Author = require("./models/Author");
 
-// Define GraphQL Schema (for now, just a simple Query)
 const typeDefs = gql`
+  type Book {
+    id: ID!
+    title: String!
+    genre: String
+    author: Author
+  }
+
+  type Author {
+    id: ID!
+    name: String!
+    age: Int
+    books: [Book]
+  }
+
   type Query {
-    message: String
+    books: [Book]
+    book(id: ID!): Book
+    authors: [Author]
+    author(id: ID!): Author
+  }
+
+  type Mutation {
+    addAuthor(name: String!, age: Int): Author
+    addBook(title: String!, genre: String, authorId: ID!): Book
   }
 `;
 
-// Define Resolvers
 const resolvers = {
   Query: {
-    message: () => "Hello, GraphQL!",
+    books: async () => await Book.find(),
+    book: async (_, { id }) => await Book.findById(id),
+    authors: async () => await Author.find(),
+    author: async (_, { id }) => await Author.findById(id),
+  },
+  Mutation: {
+    addAuthor: async (_, { name, age }) => {
+      const author = new Author({ name, age });
+      return await author.save();
+    },
+    addBook: async (_, { title, genre, authorId }) => {
+      const book = new Book({ title, genre, authorId });
+      return await book.save();
+    },
+  },
+  Book: {
+    author: async (book) => await Author.findById(book.authorId),
+  },
+  Author: {
+    books: async (author) => await Book.find({ authorId: author.id }),
   },
 };
 
